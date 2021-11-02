2793cf1f0509e6e28a47cfbcf38043b175d85141
alex22866 <isaeva85sasha@gmail.com>
2021-11-02 22:03:18 +0600

    жаба

---
+++ main.py
@@ -0,0 +1,86 @@
+# b = "hello world!"
+# for i in a:
+#   print(i)
+
+
+# for i in b:
+#     if i != '!':
+#         print(i)
+# else:
+#     print('! не существует')
+
+# a = [1, 2, 3, 4, 5]
+# b = 'hello world!'
+# food = ["манты", "борщ", "пирог", "шорпо"]
+# 1) вевысти я не ем борщ если есть в списке
+# 2) отлично вкусные эдементы списка
+# 3) если в списке нет борща вывести"классно что не было борща"
+# 4) ужин был шикарным
+
+
+# for i in food:
+#     if i == "борщ":
+#         print("я не ем борщ")
+#         #break #прекращает цикл
+#         continue #продолжает цикл
+#     print("отлично, вкусные" + i)
+# else:
+#     print("классно что не было борща")
+
+# s = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+# fib = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+#
+#
+# for i in range(len(fib)):               #[i] - индекс
+#     print(i, fib[i])
+
+# ints = [1, 2, 3, 4, 5]
+# strs = ["1", "2", "3", "4", "5"]
+
+# for i in ints:
+#   i = 1
+#  for e in strs:
+#     e = '1'
+#    print(str(i) +e)
+
+# for i in range(1, 11):
+#     print('3' + '*' + str(i) + '=' + str(3 * i))
+
+
+# def summ_ints():
+#     x = int(input()) + 1
+#     y = 0
+#     for i in range(x):
+#         y = y + i
+#     print(y)
+
+
+# object = {"минск": "беларусь","москва": "россия","киев": "украина"}
+# print(object['москва'])
+# for i in object:
+#   print(i, '- это', object[i])
+
+
+# ab = 0
+# while ab < 100:
+#     ab += 1
+#     print(ab)
+
+# ab = 11
+# while ab <= 33:
+#     ab += 1
+#     print(ab)
+
+# ab = 0
+# while ab < 100:
+#     ab -=2
+#     print(ab)
+
+
+# ab = list(range(1,101))
+# while i in ab:
+#     print(ab)
+
+for i in i:
+    if i % 2 == 0:
+     print(i if i % 2 == 0 else None)
